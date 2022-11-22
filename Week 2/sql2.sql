90cfb9e9cff184883537e5a7fb3d5a42c7be8740
Jay <94859351+jays56@users.noreply.github.com>
2022-11-22 10:26:09 +0530

    Create sql2.sql

---
+++ Week 2/sql2.sql
@@ -0,0 +1,24 @@
+1. create table students(Id varchar(20), Name varchar(20), Email varchar(40), Phone_num int, DOB DATE, Address varchar(40), Height_in_cm float(8), DeptNo varchar(20));
+   insert into students values('S0001','Abey','U2007002@rajagiri.edu.in', 9645765552 ,'15/DEC/2001', 'RSET-AID', 160, 'D004');
+   insert into students values('S0002','Jesto','U2007068@rajagiri.edu.in', 9446907495 ,'24/APR/2001', 'RSET-IT', 165, 'D002');
+   insert into students values('S0003','Adarsh','U2007003@rajagiri.edu.in', 9446168018,'10/JAN/2003', 'RSET-AID', 180, 'D004');
+   insert into students values('S0004','Kevin','U2007004@rajagiri.edu.in', 9048779636 ,'10/OCT/2002', 'RSET-IT', 175, 'D002');
+   insert into students values('S0005','Bony','U2007005@rajagiri.edu.in', 8848647102  ,'11/APR/2001', 'RSET-CSE', 168, 'D001');
+   insert into students values('S0006','Manu','U2007006@rajagiri.edu.in', 9447026251 ,'19/JUN/2003', 'RSET-CSBS', 190, 'D002');
+
+2. create table Dept(DeptNo varchar(10), DeptName varchar(20), Location varchar(20));
+   insert into Dept values('D001','DIT','A block');
+   insert into Dept values('D002','DCS','B block');
+   insert into Dept values('D003','DEC','C block');
+   insert into Dept values('D004','DEE','D block');
+
+3. create table Client(CNo varchar2(26), Name varchar2(20), City varchar2(15), PinCode number(8), State varchar2(15), Baldue number(10,2));
+
+4. insert into Client values('C001','Riya','Kochi',680324,'Kerala',14000);
+   insert into Client values('C002','Susan','Kochi',680324,'Kerala',0);
+   insert into Client values('C003','Anitha','Chennai',680345,'TamilNadu',5000);
+   insert into Client values('C004','Kumar','Pune',680223,'Maharashatra',500);
+   insert into Client values('C005','Anu','Nilampur',680321,'Kerala',7600);
+   insert into Client values('C006','Vishnu','Bombay',682111,'Maharashtraa',2055);
+
+5. 
